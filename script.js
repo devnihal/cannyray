0fede2190853c0a09194bec51f221ef85b0bbfd6
devnihal <mnaaksd2@gmail.com>
2025-01-17 09:52:27 +0530

    ASDF

--- script.js
+++ script.js
@@ -5,5 +5,7 @@ document.onscroll=()=>{
         let y = home.getBoundingClientRect().top;
         homebg.style.opacity=(Math.abs(y)/window.innerHeight)+0.25;
         homebg.style.setProperty("--blur",((Math.abs(y)/window.innerHeight)*15)+5)
+        let nav2 = document.querySelector(".navigation2");
+        nav2.style.opacity=Math.pow((Math.abs(y)/window.innerHeight),2);
     }
 }
